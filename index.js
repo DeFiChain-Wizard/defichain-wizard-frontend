3df6f8cbdb27a323c8bafc935492cd8c751e217b
Daniel Zwarg <daniel.zwarg@nsp.agency>
2022-09-25 21:17:36 +0200

    fix: disable codepush for local development

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const CodePushedApp = codePush({
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(CodePushedApp);
+registerRootComponent(__DEV__ ? App : CodePushedApp);
